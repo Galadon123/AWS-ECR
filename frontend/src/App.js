396e7d8b051bc38efe1552a3f32a3477f95e9712
Fazlul Karim <fazlulkarim362@gmail.com>
2024-09-17 08:58:44 +0600

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState('Loading...');
+
+  const callAPI = async () => {
+    try {
+      const res = await fetch("http://backend-local/");
+      const data = await res.json();
+      setData(data.message);
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Backend Response:</h1>
+      <p>{data}</p>
+    </div>
+  );
+}
+
+export default App;
